80f3f57cb865ab265bee12e4e480a0fe74a2ad8c
volodie <119340399+volodie@users.noreply.github.com>
2022-12-06 18:08:36 +0300

    Second Lab

---
+++ calendar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calendar
+{
+    internal class Program
+    {
+        enum DayOfWeek
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday ,
+            Friday ,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня недели: ");
+            string stringNumberOfDay = Console.ReadLine();
+            int NumberOfDay = Convert.ToInt32(stringNumberOfDay);
+
+            if (Enum.IsDefined(typeof(DayOfWeek), NumberOfDay))
+            {
+                Console.WriteLine((DayOfWeek)NumberOfDay);
+            }
+            else
+            {
+                Console.WriteLine(("Введен неверный номер дня недели!!!")); 
+            }   
+            Console.ReadKey();
+        }
+    }
+}
